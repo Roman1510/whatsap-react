7412ee54e6b950862a7e7f804b3b1ad1bafe7f1c
Roman1510 <romanvinnick@gmail.com>
2022-04-11 12:20:50 +0200

    created an app body

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.style.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="app__body">
+        {/* sidebar */}
+        {/* Chat */}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDDx-w5I4W0o3wA6Qz-OlC3X0Lnbh0l164',
+  authDomain: 'whatsapp-clone-7bef8.firebaseapp.com',
+  projectId: 'whatsapp-clone-7bef8',
+  storageBucket: 'whatsapp-clone-7bef8.appspot.com',
+  messagingSenderId: '473125317979',
+  appId: '1:473125317979:web:d0ca64d6690af588463e7e',
+  measurementId: 'G-4VYGDZZZND',
+}
